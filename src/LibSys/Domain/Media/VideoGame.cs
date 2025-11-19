d06a2a17f947034a5b7b4c5440a170d879ee2730
Magnus morup <magnus@morup.dk>
2025-11-19 11:43:15 +0100

    Added: Media and IDownloadable

--- src/LibSys/Domain/Media/VideoGame.cs
+++ src/LibSys/Domain/Media/VideoGame.cs
@@ -3,10 +3,20 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
     using System.Text;
     using System.Threading.Tasks;
 
     internal class VideoGame
     {
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public List<string> SupportedPlatforms { get; set; }
+         
+
+        public VideoGame()
+        {
+            SupportedPlatforms = new List<string>();
+        }
     }
 }
