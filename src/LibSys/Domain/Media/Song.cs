5b7928a6c943d4afc675fb373febc054538f04e8
NoahBR <47004125+Noahr-DK@users.noreply.github.com>
2025-11-19 13:30:08 +0100

    Update Song.cs

--- src/LibSys/Domain/Media/Song.cs
+++ src/LibSys/Domain/Media/Song.cs
@@ -1,4 +1,6 @@
-﻿namespace LibSys.Domain.Media
+﻿using LibSys.Domain.Interfaces;
+
+namespace LibSys.Domain.Media
 {
     using System;
     using System.Collections.Generic;
@@ -6,7 +8,32 @@
     using System.Text;
     using System.Threading.Tasks;
 
-    public class Song
+    public class Song : Media, IOpenable
     {
+        public string Singer { get; set; }
+        public string Genre { get; set; }
+        public string FileType {get; set;}
+        public string LocalFilePath { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string Language { get; set; }
+        
+        public Song(string title, string Singer, string Genre, string FileType, TimeSpan Duration, string Language) : base(title)
+            {
+            this.Singer = Singer;
+            this.Genre = Genre;
+            this.FileType = FileType;
+            this.Duration = Duration;
+            this.Language = Language;
+            }
+
+        public override string getDesc()
+        {
+            return $" {Title} | Song by: {Singer}| Genre: {Genre} | FileType: {FileType} | LocalFilePath: {LocalFilePath}";
+        }
+
+        public void Open()
+        {
+                throw new NotImplementedException();
+        }
     }
 }
