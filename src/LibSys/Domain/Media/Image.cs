38957cbd3a23008af9fa047c4e595a5382d27c6a
NoahBR <47004125+Noahr-DK@users.noreply.github.com>
2025-11-19 11:58:34 +0100

    Inherit Media and IOpenable in Image and Movie
    The Image and Movie classes now inherit from Media and implement the IOpenable interface, enabling shared functionality and interface compliance.

--- src/LibSys/Domain/Media/Image.cs
+++ src/LibSys/Domain/Media/Image.cs
@@ -6,7 +6,7 @@
     using System.Text;
     using System.Threading.Tasks;
 
-    public class Image
+    public class Image : Media, IOpenable
     {
     }
 }
--- src/LibSys/Domain/Media/Movie.cs
+++ src/LibSys/Domain/Media/Movie.cs
@@ -6,7 +6,7 @@
     using System.Text;
     using System.Threading.Tasks;
 
-    public class Movie
+    public class Movie : Media, IOpenable
     {
     }
 }
