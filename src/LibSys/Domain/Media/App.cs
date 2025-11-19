878f4c2e9fe3547cdf5b1dbb2aba13813fa531ed
NoahBR <47004125+Noahr-DK@users.noreply.github.com>
2025-11-19 11:27:57 +0100

    Add Execute method and LocalFilePath to IExecutable
    Introduces the Execute method and LocalFilePath field to the IExecutable interface. Updates App class to implement Execute and adds a Publisher property. Also fixes a syntax error in getDesc method.

--- src/LibSys/Domain/Media/App.cs
+++ src/LibSys/Domain/Media/App.cs
@@ -9,14 +9,15 @@
     internal class App : Media, IExecutable 
     {
         public override int Title {get; set;}
-        public int Vesion { get; set; }
+        public string Publisher {get; set;}
+        public int Vesion { get; set; } 
         public string LocalFilePath { get; set; }
         public List<string> SupportedPlatforms { get; set; }
         public int FileSize { get; set; }
 
         public override string getDesc()
         {
-            return: $"App: {Title} Version: {Version} SupportedPlatforms: {SupportedPlatforms} FileSize: {FileSize}";
+            return $"App: {Title} Version: {Version} SupportedPlatforms: {SupportedPlatforms} FileSize: {FileSize}";
         }
 
         public override void Execute()
